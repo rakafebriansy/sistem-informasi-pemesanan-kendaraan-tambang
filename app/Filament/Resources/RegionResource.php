af004a832b088c4c92029dbce41cdd31abac1d95
rakafebriansy <rakafebrian19@gmail.com>
2025-06-19 20:46:09 +0700

    feat: region resource

--- app/Filament/Resources/RegionResource.php
+++ app/Filament/Resources/RegionResource.php
@@ -6,9 +6,12 @@ use App\Filament\Resources\RegionResource\Pages;
 use App\Filament\Resources\RegionResource\RelationManagers;
 use App\Models\Region;
 use Filament\Forms;
+use Filament\Forms\Components\Grid;
+use Filament\Forms\Components\TextInput;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
 use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
@@ -17,13 +20,18 @@ class RegionResource extends Resource
 {
     protected static ?string $model = Region::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-map';
 
     public static function form(Form $form): Form
     {
         return $form
             ->schema([
-                //
+                Grid::make(2)->schema([
+                    TextInput::make('name')
+                        ->label('Nama Daerah')
+                        ->required()
+                        ->maxLength(255),
+                ]),
             ]);
     }
 
@@ -31,13 +39,18 @@ class RegionResource extends Resource
     {
         return $table
             ->columns([
-                //
+                TextColumn::make('name')->searchable()->label('Nama Daerah'),
+                TextColumn::make('created_at')
+                    ->label('Dibuat')
+                    ->dateTime('d M Y H:i')
+                    ->sortable(),
             ])
             ->filters([
                 //
             ])
             ->actions([
                 Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
@@ -60,5 +73,13 @@ class RegionResource extends Resource
             'create' => Pages\CreateRegion::route('/create'),
             'edit' => Pages\EditRegion::route('/{record}/edit'),
         ];
+    }
+    public static function getModelLabel(): string
+    {
+        return 'Daerah';
+    }
+    public static function getPluralModelLabel(): string
+    {
+        return 'Daerah';
     }
 }
