61c6e65e92fdb586e164bbc1c9f4f5e98651f7a6
rakafebriansy <rakafebrian19@gmail.com>
2025-06-19 20:51:00 +0700

    feat: vehicle type resource

---
+++ app/Filament/Resources/VehicleTypeResource.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\VehicleTypeResource\Pages;
+use App\Filament\Resources\VehicleTypeResource\RelationManagers;
+use App\Models\VehicleType;
+use Filament\Forms;
+use Filament\Forms\Components\Grid;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class VehicleTypeResource extends Resource
+{
+    protected static ?string $model = VehicleType::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-tag';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Grid::make(2)->schema([
+                    TextInput::make('name')
+                        ->label('Nama Tipe Kendaraan')
+                        ->required()
+                        ->maxLength(255),
+                ]),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')->searchable()->label('Nama Tipe Kendaraan'),
+                TextColumn::make('created_at')
+                    ->label('Dibuat')
+                    ->dateTime('d M Y H:i')
+                    ->sortable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListVehicleTypes::route('/'),
+            'create' => Pages\CreateVehicleType::route('/create'),
+            'edit' => Pages\EditVehicleType::route('/{record}/edit'),
+        ];
+    }
+    public static function getModelLabel(): string
+    {
+        return 'Tipe Kendaraan';
+    }
+    public static function getPluralModelLabel(): string
+    {
+        return 'Tipe Kendaraan';
+    }
+}
